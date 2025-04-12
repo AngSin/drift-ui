fa566792a8ea9a7ebbb4155571414a85be9154d5
angadsingh <angad17@hotmail.com>
2025-04-12 16:45:31 +0100

    Add links to navbar

--- src/app/Navbar.tsx
+++ src/app/Navbar.tsx
@@ -3,32 +3,39 @@ import {useWallet} from '@solana/wallet-adapter-react';
 import {WalletMultiButton} from "@solana/wallet-adapter-react-ui";
 // import {getAssociatedTokenAddress} from "@solana/spl-token";
 import AccountsDrawer from "@/app/AccountsDrawer";
+import {Link} from "@chakra-ui/react";
 
 const Navbar = () => {
   const { connected, connecting, publicKey: userPubKey } = useWallet();
 
 
   return (
-    <div className="flex justify-end w-full">
-      <WalletMultiButton>
-        {connected ? (
-          <span className="flex items-center gap-2">
-            {userPubKey?.toBase58().slice(0, 6)}...
-            {userPubKey?.toBase58().slice(-4)}
-              </span>
-        ) : (
-          <span className="flex items-center gap-2">
-                {connecting ? (
-                  'Connecting...'
-                ) : (
-                  <>
-                    Connect Wallet
-                  </>
-                )}
-              </span>
-        )}
-      </WalletMultiButton>
-      <AccountsDrawer />
+    <div className="flex justify-between w-full">
+      <div className="flex justify-start gap-2">
+        <Link href="/">Trade</Link>
+        <Link href="/explore">Explore</Link>
+      </div>
+      <div className="flex justify-end">
+        <WalletMultiButton>
+          {connected ? (
+            <span className="flex items-center gap-2">
+              {userPubKey?.toBase58().slice(0, 6)}...
+              {userPubKey?.toBase58().slice(-4)}
+                </span>
+          ) : (
+            <span className="flex items-center gap-2">
+                  {connecting ? (
+                    'Connecting...'
+                  ) : (
+                    <>
+                      Connect Wallet
+                    </>
+                  )}
+                </span>
+          )}
+        </WalletMultiButton>
+        <AccountsDrawer />
+      </div>
     </div>
   );
 };
